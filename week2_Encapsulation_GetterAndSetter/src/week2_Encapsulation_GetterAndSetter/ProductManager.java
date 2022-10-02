530409429a7375542b4f1cd80e78f421c7bb4584
Mahmut Esat <81807305+mahmut-esat@users.noreply.github.com>
2022-10-02 17:29:22 +0300

    Add files via upload

---
+++ week2_Encapsulation_GetterAndSetter/src/week2_Encapsulation_GetterAndSetter/ProductManager.java
@@ -0,0 +1,9 @@
+package week2_Encapsulation_GetterAndSetter;
+
+public class ProductManager {
+
+	public void Add(Product product) {
+		System.out.println("Ürün eklendi: " + product.getName());
+	}
+	
+}
